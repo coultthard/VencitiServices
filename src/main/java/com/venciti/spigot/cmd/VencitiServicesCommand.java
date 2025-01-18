e7569790215f10e0d84a9c37c4befbf842eea1fd
Sqlitt <118855875+sqlitt@users.noreply.github.com>
2025-01-18 03:15:52 -0300

    rebuild

--- src/main/java/com/venciti/spigot/cmd/VencitiServicesCommand.java
+++ src/main/java/com/venciti/spigot/cmd/VencitiServicesCommand.java
@@ -1,6 +1,8 @@
 package com.venciti.spigot.cmd;
 
+import com.venciti.spigot.Services;
 import com.venciti.spigot.api.DownloadAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -22,6 +24,14 @@ public class VencitiServicesCommand extends Commands{
         if (!player.hasPermission("venciti.invsee") || !player.isOp()) {
             player.sendMessage("§cVocê não possui cargo o suficiente para executar este comando.");
             return;
+        }
+
+        String autors = Services.getInstance().getDescription().getAuthors().toString();
+        if (args.length == 0){
+            player.sendMessage("§e");
+            player.sendMessage(" §eVencitiServirces");
+            player.sendMessage(" §fCriador: §7" + autors);
+            player.sendMessage("§e");
         }
 
         if (args[0].equalsIgnoreCase("download")){
